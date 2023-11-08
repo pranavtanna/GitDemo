6ef43421b3e89f67889c33c81bb09ca822d7669d
USER <pranavdtanna@gmail.com>
2023-11-08 18:03:18 +0530

    This is from GitStuffX

--- src/test/java/org/rahulshettyacademy/eCommerce_tc_2.java
+++ src/test/java/org/rahulshettyacademy/eCommerce_tc_2.java
@@ -18,26 +18,20 @@ import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.Activity;
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 
 public class eCommerce_tc_2 extends AndroidBaseTest{
 
-	
-	
 	@BeforeMethod
 	public void preSetup()
 	{
 		//screen to home page
 		Activity activity = new Activity("com.androidsample.generalstore", "com.androidsample.generalstore.MainActivity");
-		driver.startActivity(activity);
-		
-		
+		driver.startActivity(activity);	
 	}
-	
 	@Test
-
 	public void FillForm_ErrorValidation() throws InterruptedException
 	{
-		
 	//	driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Rahul Shetty");
 		driver.hideKeyboard();
 		driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
@@ -46,19 +40,13 @@ public class eCommerce_tc_2 extends AndroidBaseTest{
 		driver.findElement(By.xpath("//android.widget.TextView[@text='Argentina']")).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
 		String toastMessage = driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
-		AssertJUnit.assertEquals(toastMessage,"Please your name");
-		
-		
-			
+		AssertJUnit.assertEquals(toastMessage,"Please your name");	
 	}
 	
 
 	@Test
 	public void FillForm_PositiveFlow() throws InterruptedException
-
-	
 	{
-		
 		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Rahul Shetty");
 		driver.hideKeyboard();
 		driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
@@ -66,8 +54,13 @@ public class eCommerce_tc_2 extends AndroidBaseTest{
 		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));"));
 		driver.findElement(By.xpath("//android.widget.TextView[@text='Argentina']")).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
-		AssertJUnit.assertTrue(driver.findElements(By.xpath("(//android.widget.Toast)[1]")).size()<1);
-		
-			
+		AssertJUnit.assertTrue(driver.findElements(By.xpath("(//android.widget.Toast)[1]")).size()<1);	
+	}
+	
+	public void GitDemo()
+	{
+		System.out.println("hi practicing git commands");
+		System.out.println("Practice going good");
+		System.out.println("Done with the practice");
 	}
 }
